db5e12c6fd631bb627a24ec4d2383be3d8109ca3
Itay Dali <itaydali8@gmail.com>
2024-03-31 21:09:50 +0300

    Added an infinite loop in the Server::run() function that gets commands from the admin

--- Trivia/Server.cpp
+++ Trivia/Server.cpp
@@ -18,4 +18,14 @@ void Server::run()
 {
 	_commThread = new std::thread(&Communicator::startHandleRequests,
 		_communicator);
+	std::string inp;
+	do
+	{
+		std::cout << "Enter command (EXIT to close program):" << std::endl;
+		std::cin >> inp;
+
+		// IMPLEMENT ADMIN COMMANDS HERE
+
+	} while (inp != "EXIT");
+	exit(0);
 }
