ca8d84c9916f9562426c9cf147c3a196bfb7afc9
Lior G <liorgleizer1@gmail.com>
2024-03-31 20:05:26 +0300

    Declared functions

---
+++ Trivia/Communicator.cpp
@@ -0,0 +1,21 @@
+#include "Communicator.h"
+
+void Communicator::bindAndListen()
+{
+}
+
+void Communicator::handleNewClient(SOCKET clientSocket)
+{
+}
+
+Communicator::Communicator()
+{
+}
+
+Communicator::~Communicator()
+{
+}
+
+void Communicator::startHandleRequests()
+{
+}
