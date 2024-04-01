983cd20dce743bc781c8955ca239ccf1a2167560
Itay Dali <itaydali8@gmail.com>
2024-04-01 16:57:06 +0300

    Implemented all serializeResponse functions

--- Trivia/JsonRequestPacketSerializer.cpp
+++ Trivia/JsonRequestPacketSerializer.cpp
@@ -19,10 +19,30 @@ Buffer JsonRequestPacketSerializer::serializeResponse(ErrorResponse res)
 
 Buffer JsonRequestPacketSerializer::serializeResponse(LoginResponse res)
 {
-    return Buffer();
+    json jsonObj{ { "status", res.status } };
+
+    auto msg = jsonObj.dump();
+    int msgSize = msg.length();
+
+    Buffer buff;
+    buff.resize(msgSize + HEADER_FIELD_LENGTH); // another byte for a response code and message size)
+    buff[0] = LoginResponseCode; // no need for memcpy for a size of 1
+    std::memcpy(&buff[CODE_FIELD_LENGTH], &msgSize, SIZE_FIELD_LENGTH);
+    std::memcpy(&buff[HEADER_FIELD_LENGTH], msg.c_str(), msgSize);
+    return buff;
 }
 
 Buffer JsonRequestPacketSerializer::serializeResponse(SignupResponse res)
 {
-    return Buffer();
+    json jsonObj{ { "status", res.status } };
+
+    auto msg = jsonObj.dump();
+    int msgSize = msg.length();
+
+    Buffer buff;
+    buff.resize(msgSize + HEADER_FIELD_LENGTH); // another byte for a response code and message size)
+    buff[0] = SignupResponseCode; // no need for memcpy for a size of 1
+    std::memcpy(&buff[CODE_FIELD_LENGTH], &msgSize, SIZE_FIELD_LENGTH);
+    std::memcpy(&buff[HEADER_FIELD_LENGTH], msg.c_str(), msgSize);
+    return buff;
 }
