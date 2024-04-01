38139e1c0c31586e98cbe7ab4a09184b6fd70598
Itay Dali <itaydali8@gmail.com>
2024-04-01 17:04:56 +0300

    Implemented a private static function buildBuffer instead of copying and pasting code

--- Trivia/JsonRequestPacketSerializer.cpp
+++ Trivia/JsonRequestPacketSerializer.cpp
@@ -1,47 +1,34 @@
 #include "JsonRequestPacketSerializer.h"
 
-using json = nlohmann::json;
-
 Buffer JsonRequestPacketSerializer::serializeResponse(ErrorResponse res)
 {
     json jsonObj{ { "message", res.message } };
     
-    auto msg = jsonObj.dump();
-    int msgSize = msg.length();
-
-    Buffer buff;
-    buff.resize(msgSize + HEADER_FIELD_LENGTH); // another byte for a response code and message size)
-    buff[0] = ErrorResponseCode; // no need for memcpy for a size of 1
-    std::memcpy(&buff[CODE_FIELD_LENGTH], &msgSize, SIZE_FIELD_LENGTH);
-    std::memcpy(&buff[HEADER_FIELD_LENGTH], msg.c_str(), msgSize);
-    return buff;
+    return buildBuffer(ErrorResponseCode, jsonObj);
 }
 
 Buffer JsonRequestPacketSerializer::serializeResponse(LoginResponse res)
 {
     json jsonObj{ { "status", res.status } };
 
-    auto msg = jsonObj.dump();
-    int msgSize = msg.length();
-
-    Buffer buff;
-    buff.resize(msgSize + HEADER_FIELD_LENGTH); // another byte for a response code and message size)
-    buff[0] = LoginResponseCode; // no need for memcpy for a size of 1
-    std::memcpy(&buff[CODE_FIELD_LENGTH], &msgSize, SIZE_FIELD_LENGTH);
-    std::memcpy(&buff[HEADER_FIELD_LENGTH], msg.c_str(), msgSize);
-    return buff;
+    return buildBuffer(LoginResponseCode, jsonObj);
 }
 
 Buffer JsonRequestPacketSerializer::serializeResponse(SignupResponse res)
 {
     json jsonObj{ { "status", res.status } };
 
+    return buildBuffer(SignupResponseCode, jsonObj);
+}
+
+Buffer JsonRequestPacketSerializer::buildBuffer(MessageCode resCode, const json& jsonObj)
+{
     auto msg = jsonObj.dump();
     int msgSize = msg.length();
 
-    Buffer buff;
-    buff.resize(msgSize + HEADER_FIELD_LENGTH); // another byte for a response code and message size)
-    buff[0] = SignupResponseCode; // no need for memcpy for a size of 1
+    Buffer buff(msgSize + HEADER_FIELD_LENGTH);
+
+    buff[0] = resCode; // no need for memcpy for a size of 1
     std::memcpy(&buff[CODE_FIELD_LENGTH], &msgSize, SIZE_FIELD_LENGTH);
     std::memcpy(&buff[HEADER_FIELD_LENGTH], msg.c_str(), msgSize);
     return buff;
--- Trivia/JsonRequestPacketSerializer.h
+++ Trivia/JsonRequestPacketSerializer.h
@@ -3,10 +3,15 @@
 #include "Responses.h"
 #include "Constants.h"
 
+using json = nlohmann::json;
+
 class JsonRequestPacketSerializer
 {
 public:
 	static Buffer serializeResponse(ErrorResponse res);
 	static Buffer serializeResponse(LoginResponse res);
 	static Buffer serializeResponse(SignupResponse res);
+
+private:
+	static Buffer buildBuffer(MessageCode resCode, const json& jsonObj);
 };
