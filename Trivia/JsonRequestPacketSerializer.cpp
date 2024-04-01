dd59c5a0d8ee1f8e5fef2132e586bc196308cd8d
Lior G <liorgleizer1@gmail.com>
2024-04-01 22:54:05 +0300

    Use enum class correctly

--- Trivia/JsonRequestPacketSerializer.cpp
+++ Trivia/JsonRequestPacketSerializer.cpp
@@ -4,21 +4,21 @@ Buffer JsonRequestPacketSerializer::serializeResponse(ErrorResponse res)
 {
     json jsonObj{ { "message", res.message } };
     
-    return buildBuffer(ErrorResponseCode, jsonObj);
+    return buildBuffer(MessageCode::ErrorResponseCode, jsonObj);
 }
 
 Buffer JsonRequestPacketSerializer::serializeResponse(LoginResponse res)
 {
     json jsonObj{ { "status", res.status } };
 
-    return buildBuffer(LoginResponseCode, jsonObj);
+    return buildBuffer(MessageCode::LoginResponseCode, jsonObj);
 }
 
 Buffer JsonRequestPacketSerializer::serializeResponse(SignupResponse res)
 {
     json jsonObj{ { "status", res.status } };
 
-    return buildBuffer(SignupResponseCode, jsonObj);
+    return buildBuffer(MessageCode::SignupResponseCode, jsonObj);
 }
 
 Buffer JsonRequestPacketSerializer::buildBuffer(MessageCode resCode, const json& jsonObj)
@@ -28,7 +28,7 @@ Buffer JsonRequestPacketSerializer::buildBuffer(MessageCode resCode, const json&
 
     Buffer buff(msgSize + HEADER_FIELD_LENGTH);
 
-    buff[0] = resCode; // no need for memcpy for a size of 1
+    buff[0] = (int)resCode; // no need for memcpy for a size of 1
     std::memcpy(&buff[CODE_FIELD_LENGTH], &msgSize, SIZE_FIELD_LENGTH);
     std::memcpy(&buff[HEADER_FIELD_LENGTH], msg.c_str(), msgSize);
     return buff;
