83098906b71b304343a7e10040d2150aa165ed06
Lior G <liorgleizer1@gmail.com>
2024-04-02 22:14:00 +0300

    Header files for database, class definition and sqlite3 library

---
+++ Trivia/IDatabase.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+
+class IDatabase {
+public:
+	/* Public Queries*/
+	virtual void AddUser(const std::string& username, const std::string& password, const std::string& email) = 0;
+
+	/* Private Queries */
+	virtual bool DoesUserExist(const std::string& username) = 0;
+	virtual bool IsPasswordOk(const std::string& password) = 0;
+};
---
+++ Trivia/SqliteDatabase.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Lib/sqlite3.h"
+#include "IDatabase.h"
+#include "Constants.h"
+
+class SqliteDatabase : public IDatabase {
+public:
+	SqliteDatabase(const std::string& dbName);
+	~SqliteDatabase();
+
+	void AddUser(const std::string& username, const std::string& password, const std::string& email) override;
+
+private:
+	/* Private Members */
+	sqlite3* _db;
+
+	/* Private Queries */
+	bool DoesUserExist(const std::string& username) override;
+	bool IsPasswordOk(const std::string& password) override;
+};
