6f45c2221b6bc119811eaba231b2db9efb2210a5
Itay Dali <itaydali8@gmail.com>
2024-04-02 22:19:56 +0300

    Edited the python tests to meet the version

--- BaseServerTesting/serverTest.py
+++ BaseServerTesting/serverTest.py
@@ -1,12 +1,18 @@
 import socket
+import json
 
+PORT = 6969
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("127.0.0.1", 6969))
+    s.connect(("127.0.0.1", PORT))
+    print("Connected successfully to port", PORT)
 
-    s.sendall("Hello".encode())
+    msg = json.dumps({ "username": "username", "password": "1234567" })
+    packet = b'\x00' + len(msg).to_bytes(4, 'big') + msg.encode('ascii')
 
+    s.sendall(packet)
+    print("Recieved:", s.recv(1024))
     s.close()
 
 
