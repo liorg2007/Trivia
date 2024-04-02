3981092ecf811a5e84083147f461bda0df07195f
Itay Dali <itaydali8@gmail.com>
2024-04-02 20:58:03 +0300

    Implemented LoginRequestHandler - handleRequest function

--- Trivia/LoginRequestHandler.cpp
+++ Trivia/LoginRequestHandler.cpp
@@ -4,9 +4,25 @@
 
 bool LoginRequestHandler::isRequestRelevant(const RequestInfo& req)
 {
-    return req.id == MessageCode::LoginRequestCode;
+	return req.id == MessageCode::LoginRequestCode;
 }
 
 RequestResult LoginRequestHandler::handleRequest(const RequestInfo& req)
 {
+	LoginRequest login = JsonRequestPacketDeserializer::deserializeLoginRequest(req.buffer);
+	RequestResult reqResult;
+	LoginResponse res;
+
+	// check if username matches password according to the database
+	if (true) 
+	{
+		res.status = 1;
+	}
+	else
+	{
+		res.status = 0;
+	}
+	reqResult.newHandler = new LoginRequestHandler(); // the next state, not LoginRequestHandler
+	reqResult.response = JsonRequestPacketSerializer::serializeResponse(res);
+	return reqResult;
 }
