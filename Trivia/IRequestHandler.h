9a621d1ffbfb0e1b3aad89a6164bbd9cfb9c28f2
Itay Dali <itaydali8@gmail.com>
2024-04-02 10:11:28 +0300

    IRequestHandler interface

--- Trivia/IRequestHandler.h
+++ Trivia/IRequestHandler.h
@@ -1,5 +1,8 @@
 #pragma once
+#include "Requests.h"
 
 class IRequestHandler {
-
+protected:
+	virtual bool isRequestRelevant(const RequestInfo& reqInfo) = 0;
+	virtual RequestResult handleRequest(const RequestInfo& reqInfo) = 0;
 };
