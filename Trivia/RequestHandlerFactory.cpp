6a6b5944c252495e9235e05f9a70ff9f2acbe9a1
Itay Dali <itaydali8@gmail.com>
2024-04-03 21:38:18 +0300

    started implementing RequestHandlerFactory cant continue since LoginRequestHandler isnt ready

--- Trivia/RequestHandlerFactory.cpp
+++ Trivia/RequestHandlerFactory.cpp
@@ -1,1 +1,16 @@
 #include "RequestHandlerFactory.h"
+
+RequestHandlerFactory::RequestHandlerFactory(IDatabase* database)
+    : _database(database), _loginManager(database)
+{
+}
+
+LoginRequestHandler* RequestHandlerFactory::createLoginRequestHandler() const
+{
+    return nullptr;
+}
+
+LoginManager& RequestHandlerFactory::getLoginManager()
+{
+    return _loginManager;
+}
--- Trivia/RequestHandlerFactory.h
+++ Trivia/RequestHandlerFactory.h
@@ -1,12 +1,15 @@
 #pragma once
 #include "LoginRequestHandler.h"
 #include "IDatabase.h"
+#include "LoginManager.h"
 
 class RequestHandlerFactory
 {
 public:
+	RequestHandlerFactory(IDatabase* database);
+
 	LoginRequestHandler* createLoginRequestHandler() const;
-	LoginManager& getLoginManager() const;
+	LoginManager& getLoginManager();
 
 private:
 	LoginManager _loginManager;
