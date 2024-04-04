0033f252a67dde9eb03fee27a3b957ef024cfc2c
Lior G <liorgleizer1@gmail.com>
2024-04-04 18:54:46 +0300

    Corrected logic

--- Trivia/LoginManager.cpp
+++ Trivia/LoginManager.cpp
@@ -19,7 +19,7 @@ bool LoginManager::signup(const std::string& username, const std::string& passwo
 bool LoginManager::login(const std::string& username, const std::string& password)
 {
 	LoggedUser newLoggedUser(username);
-	if (std::find(_loggedUsers.begin(), _loggedUsers.end(), newLoggedUser) != _loggedUsers.end()
+	if (std::find(_loggedUsers.begin(), _loggedUsers.end(), newLoggedUser) == _loggedUsers.end()
 		&& _database->IsPasswordOk(username, password))
 	{
 		_loggedUsers.push_back(LoggedUser(username));
