8e0d79cc8f854a1e8c10c0b460fe5f514ab5d66f
Lior G <liorgleizer1@gmail.com>
2024-04-06 17:46:30 +0300

    Updated tests to send new data too

--- BaseServerTesting/loginSignupTesting.py
+++ BaseServerTesting/loginSignupTesting.py
@@ -18,7 +18,7 @@ def Test1(s):
     send_json(s, LOGIN_CODE, { "username": "blahblahblahblah", "password": "1234567" })
     print("Recieved for login with non existent user:", s.recv(1024))
 
-    send_json(s, SIGNUP_CODE, { "username": "shimon", "password": "1234567", "email": "shimon@gmail.com" })
+    send_json(s, SIGNUP_CODE, { "username": "shimon", "password": "1234567", "email": "shimon@gmail.com", "address" : "Shimon 12", "phoneNumber" : "0501231234", "birthDate" : "1/2/2007"  })
     print("Recieved for signup:", s.recv(1024))
 
     s.close()
@@ -29,12 +29,12 @@ def Test2(s):
     s.connect(("127.0.0.1", PORT))
     print("Connected successfully to port", PORT)
     
-    send_json(s, SIGNUP_CODE, { "username": "lior", "password": "1234567", "email" : "lior@hah.com" })
+    send_json(s, SIGNUP_CODE, { "username": "lior", "password": "1234567", "email" : "lior@hah.com", "address" : "Shimon 12", "phoneNumber" : "0501231234", "birthDate" : "1/2/2007" })
     print("Recieved for signup for the first time:", s.recv(1024))
 
     s2.connect(("127.0.0.1", PORT))
     print("Connected successfully to port", PORT, "with another socket")
-    send_json(s2, SIGNUP_CODE, { "username": "lior", "password": "3123123", "email" : "bbb@bruh.com" })
+    send_json(s2, SIGNUP_CODE, { "username": "lior", "password": "3123123", "email" : "bbb@bruh.com", "address" : "Shimon 12", "phoneNumber" : "0501231234", "birthDate" : "1/2/2007"  })
     print("Recieved for signup for the second time with the same username:", s2.recv(1024))
 
     s2.close()
