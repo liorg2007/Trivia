10071a0ef9a46bd7dee7fda329cc51e08e28ce23
Lior G <liorgleizer1@gmail.com>
2024-04-06 17:39:10 +0300

    LoginManager Changed

--- Trivia/LoginManager.h
+++ Trivia/LoginManager.h
@@ -9,7 +9,7 @@ class LoginManager
 public:
 	LoginManager(IDatabase* database);
 
-	bool signup(const std::string& username, const std::string& password, const std::string& email);
+	bool signup(const std::string& username, const std::string& password, const std::string& email, const std::string& address, const std::string& phoneNumber, const std::string& birthDate);
 	bool login(const std::string& username, const std::string& password);
 	bool logout(const std::string& username);
 
