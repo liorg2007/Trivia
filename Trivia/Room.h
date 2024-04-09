70cb23e14669dd726d1ad238f0885e28a2f4ebd0
Lior G <liorgleizer1@gmail.com>
2024-04-09 10:03:31 +0300

    Created class for room, and struct of RoomData

---
+++ Trivia/Room.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "RoomData.h"
+#include "LoggedUser.h"
+#include <vector>
+
+class Room {
+private:
+	RoomData _RoomData;
+	std::vector<LoggedUser> _users;
+
+public:
+	void addUser(const LoggedUser& loggedUser);
+	void removeUser(const LoggedUser& loggedUser);
+	std::vector<std::string> getAllUsers() const;
+};
