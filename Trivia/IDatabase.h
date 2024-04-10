59ae542b30c960e2839bdd3cfc15473fb841d9f9
Lior G <liorgleizer1@gmail.com>
2024-04-10 12:58:34 +0300

    Statistics Database Queries

--- Trivia/IDatabase.h
+++ Trivia/IDatabase.h
@@ -14,6 +14,15 @@ public:
 	virtual bool doesUserExist(const std::string& username) = 0;
 	virtual bool doesPasswordMatch(const std::string& username, const std::string& password) = 0;
 
+	/* Statistics Queries */
+	virtual float getPlayerAverageAnswerTime(const std::string& userName) = 0;
+	virtual int getNumOfCorrectAnswers(const std::string& userName) = 0;
+	virtual int getNumOfTotalAnswers(const std::string& userName) = 0;
+	virtual int getNumOfPlayerGames(const std::string& userName) = 0;
+	virtual int getNumOfCorrectAnswers(const std::string& userName) = 0;
+	virtual int getPlayerScore(const std::string& userName) = 0;
+	virtual std::vector<std::string> getHighScores() = 0;
+
 	IDatabase(IDatabase&) = delete;
 	void operator=(const IDatabase&) = delete;
 
