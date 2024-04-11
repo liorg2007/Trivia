e923eebbea4b0bef9c6b2e782d035b31c31ba635
Itay Dali <itaydali8@gmail.com>
2024-04-11 17:21:14 +0300

    Responses structs and message codes

--- Trivia/Responses.h
+++ Trivia/Responses.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 #include <string>
 
 struct LoginResponse
@@ -11,7 +12,45 @@ struct SignupResponse
 	unsigned int status;
 };
 
+struct LogoutResponse
+{
+	unsigned int status;
+};
+
 struct ErrorResponse
 {
 	std::string message;
+};
+
+struct GetRoomsResponse
+{
+	unsigned int status;
+	std::vector<RoomData> rooms;
+};
+
+struct GetPlayersInRoomResponse
+{
+	std::vector<std::string> players;
+};
+
+struct GetHighScoreResponse
+{
+	unsigned int status;
+	std::vector<std::string> statistics;
+};
+
+struct GetPersonalStatsResponse
+{
+	unsigned int status;
+	std::vector<std::string> statistics;
+};
+
+struct JoinRoomResponse
+{
+	unsigned int status;
+};
+
+struct CreateRoomResponse
+{
+	unsigned int status;
 };
