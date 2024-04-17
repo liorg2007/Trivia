208b035ce51e22159d33803175db402249880ba7
Lior G <liorgleizer1@gmail.com>
2024-04-17 15:48:44 +0300

    Implemented read from config file, if file doesnt exist or no 2 lines then raise error box

--- Client/App.xaml.cs
+++ Client/App.xaml.cs
@@ -1,19 +1,63 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Runtime.CompilerServices;
 using System.Windows;
+using System.IO;
+using static Client.Helper;
 
 namespace Client
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    /// 
+
+    struct ServerData
+    {
+        public string ip;
+        public int port;
+    }
+
     public partial class App : Application
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            try
+            {
+                ServerData connDetails = getServerConnData();
+            }
+            catch(Exception ex)
+            {
+                raiseErrorBox(ex.Message);
+                System.Environment.Exit(0);
+            }
+
             MainWindow mainWindow = new MainWindow();
 
             mainWindow.Show();
+        }
+
+        private ServerData getServerConnData()
+        {
+            const string serverDataFileName = "serverConfig.txt";
+            var serverData = new ServerData();
+
+            if (!File.Exists(serverDataFileName))
+                throw new Exception("No serverConfig.txt file!");
+
+            string[] data = File.ReadAllLines(serverDataFileName);
+
+            try
+            {
+                serverData.ip = data[0];
+                serverData.port = int.Parse(data[1]);
+            }
+            catch
+            {
+                throw new Exception("Can't read from config file!");
+            }
+
+            return serverData;
         }
     }
 
---
+++ Client/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client
+{
+    internal static class Helper
+    {
+        public static void raiseErrorBox(string message)
+        {
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBox.Show(message, "Error", button, icon, MessageBoxResult.Yes);
+        }
+    }
+}
