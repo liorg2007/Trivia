20464de0e49675d8c7efe210bda37a98d006334a
Lior G <liorgleizer1@gmail.com>
2024-04-29 10:30:33 +0300

    Room Selector Lobby Concept Style

---
+++ Client/Menu/RoomSelector.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client.Menu
+{
+    /// <summary>
+    /// Interaction logic for RoomSelector.xaml
+    /// </summary>
+    public partial class RoomSelector : Window
+    {
+        public RoomSelector()
+        {
+            InitializeComponent();
+        }
+
+        private void exitPress(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current)._musicWorker.CancelAsync();
+            Thread.Sleep(50);
+            Close();
+        }
+
+        private void MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
