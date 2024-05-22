afd57e44f8626942cb05c550def8031227d9833a
Itay Dali <itaydali8@gmail.com>
2024-05-22 17:01:39 +0300

    Fixed LOGIN and SIGNUP codes in the python tests

--- BaseServerTesting/serverTest.py
+++ BaseServerTesting/serverTest.py
@@ -2,19 +2,20 @@ import socket
 import json
 
 PORT = 6969
-LOGIN_CODE = 0
-SIGNUP_CODE = 1
+LOGIN_CODE = 1
+SIGNUP_CODE = 2
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("127.0.0.1", PORT))
     print("Connected successfully to port", PORT)
 
-    send_json(s, LOGIN_CODE, { "username": "username", "password": "1234567" })
+    send_json(s, SIGNUP_CODE, { "username": "mrBruh", "password": "#Aa1234567", "email": "gever@gmail.com", "address": "abc,1,ff", "phoneNumber": "0523411223", "birthDate": "14/03/2006" })
+    print("Recieved for signup:", s.recv(1024))
+    
+    send_json(s, LOGIN_CODE, { "username": "mrBruh", "password": "#Aa1234567" })
     print("Recieved for login:", s.recv(1024))
 
-    send_json(s, SIGNUP_CODE, { "username": "username", "password": "1234567", "email": "gever@gmail.com" })
-    print("Recieved for signup:", s.recv(1024))
 
     s.close()
 
