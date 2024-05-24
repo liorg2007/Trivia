887dbb0b3cf28bd9322c98385e8944fc39d49bfc
Lior G <liorgleizer1@gmail.com>
2024-05-24 22:03:13 +0300

    #8 Seperated the deserialize for start game, leave and getRoom state

--- Client/Rooms/WaitingRoomCommands.cs
+++ Client/Rooms/WaitingRoomCommands.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static Client.Requests;
+using static Client.DataStructs;
 
 namespace Client.Rooms
 {
@@ -18,14 +19,7 @@ namespace Client.Rooms
             ServerResponse response = Helper.decodeProtocol(app._server.receiveMessage());
 
             if (response.code == Code.LeaveRoom)
-            {
-                LeaveRoomResponse res = JsonPacketDeserializer.DeserializeLeaveRoomResponse(response.message);
-
-                if (res.status != 1)
-                    return false;
-                else
-                    return true;
-            }
+                return GetLeaveRoomResponse(response.message);
 
             return false;
         }
@@ -60,16 +54,49 @@ namespace Client.Rooms
             ServerResponse response = Helper.decodeProtocol(app._server.receiveMessage());
 
             if (response.code == Code.StartGame)
-            {
-                StartGameResponse res = JsonPacketDeserializer.DeserializeStartGameResponse(response.message);
-
-                if (res.status != 1)
-                    throw new Exception("Can't start game");
-                else
-                    return res.start_time;
-            }
+                return GetStartGameResponse(response.message);
+            
 
             throw new Exception("Bad response");
+        }
+
+        public static ServerResponse GetRoomStateRequest(App app)
+        {
+            var message = Helper.createProtocol(Code.StartGame);
+
+            app._server.sendMessage(message);
+
+            return Helper.decodeProtocol(app._server.receiveMessage());
+        }
+
+        public static RoomState GetRoomStateResponse(string message)
+        {
+            GetRoomStateResponse res = JsonPacketDeserializer.DeserializeGetRoomStateResponse(message);
+
+            if (res.status != 1)
+                throw new Exception("Can't get room data");
+            else
+                return res.roomState;
+        }
+
+        public static long GetStartGameResponse(string message)
+        {
+            StartGameResponse res = JsonPacketDeserializer.DeserializeStartGameResponse(message);
+
+            if (res.status != 1)
+                throw new Exception("Can't start game");
+            else
+                return res.start_time;
+        }
+
+        public static bool GetLeaveRoomResponse(string message)
+        {
+            LeaveRoomResponse res = JsonPacketDeserializer.DeserializeLeaveRoomResponse(message);
+
+            if (res.status != 1)
+                return false;
+            else
+                return true;
         }
     }
 }
