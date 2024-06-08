ba2c647917b14b4fbc7be7a2885c1f4a13756847
Lior G <liorgleizer1@gmail.com>
2024-06-08 22:18:33 +0300

    #11 isRequestRelevant, handleDisconnect handleRequest

---
+++ Trivia/GameRequestHandler.cpp
@@ -0,0 +1,27 @@
+#include "GameRequestHandler.h"
+
+bool GameRequestHandler::isRequestRelevant(const RequestInfo& reqInfo)
+{
+	return reqInfo.id == ProtocolCode::GetQuestion
+		|| reqInfo.id == ProtocolCode::SubmitAnswer
+		|| reqInfo.id == ProtocolCode::GetGameResults
+		|| reqInfo.id == ProtocolCode::LeaveGame;
+}
+
+void GameRequestHandler::handleDisconnect()
+{
+	//Don't remove from room because stats of player should be shown in the end
+	_handlerFactory.getLoginManager().logout(_user.getUsername());
+}
+
+RequestResult GameRequestHandler::handleRequest(const RequestInfo& reqInfo)
+{
+	if (reqInfo.id == ProtocolCode::GetQuestion)
+		return getQuestion(reqInfo);
+	else if (reqInfo.id == ProtocolCode::SubmitAnswer)
+		return submitAnswer(reqInfo);
+	else if (reqInfo.id == ProtocolCode::GetGameResults)
+		return getGameResults(reqInfo);
+	else
+		return leaveGame(reqInfo);
+}
