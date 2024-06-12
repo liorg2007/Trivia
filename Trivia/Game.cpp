1f5d2dba03acc45c0a5ad55c4f11e28bd6467c45
Lior G <liorgleizer1@gmail.com>
2024-06-12 11:19:10 +0300

    #11 SubmitAnswer in Game.h

--- Trivia/Game.cpp
+++ Trivia/Game.cpp
@@ -19,5 +19,21 @@ Game::Game(std::vector<std::string> players, GameDetails gameDetails)
 }
 
 void Game::submitAnswer(const LoggedUser& user, unsigned int answerId)
-{
+{ 
+	std::shared_ptr<GameData> userGameData = _players.at(user);
+
+	if (userGameData->currentQuestion == nullptr) //check that question isn't nullptr. !shouldn't happen but in case of some tampering maybe, then server won't crash
+		return;
+
+	if (std::time(nullptr) - _gameDetails.answerTimeout >= userGameData->lastSubmission) 	//check if user submitted answer in time
+		++(userGameData->wrongAnswerCount);
+	else if(userGameData->currentQuestion->getCorrectAnswerId() == answerId) 	//check if user submitted correct answer
+		++(userGameData->correctAnswerCount);
+	else
+		++(userGameData->wrongAnswerCount);
+
+
+	userGameData->lastSubmission = std::time(nullptr);
+	userGameData->averageAnswerTime = (userGameData->lastSubmission - _gameDetails.gameStartTime) / (userGameData->correctAnswerCount + userGameData->wrongAnswerCount);
+	userGameData->currentQuestion = getQuestionForUser(user);
 }
