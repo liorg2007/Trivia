798db7c5b3de8cb9ef4cc037b6bd9349681b13d4
Lior G <liorgleizer1@gmail.com>
2024-06-24 01:01:51 +0300

    new way to calculate score

--- Trivia/SqliteDatabase.cpp
+++ Trivia/SqliteDatabase.cpp
@@ -275,12 +275,16 @@ int SqliteDatabase::calculateScore(const std::string& userName)
 	execQuery(query.str(), scoreDataCallback, &scoreData);
 
 	// Avoid division by zero
-	if (scoreData.totalAnswers == 0 || scoreData.averageTime == 0) 
-		return 0; 
+	if (scoreData.totalAnswers == 0 || scoreData.averageTime == 0)
+		return 0;
 
-	double timeFunction = 1.0 / scoreData.averageTime;
+	double accuracy = static_cast<double>(scoreData.correctAnswers) / scoreData.totalAnswers;
+	double timeFactor = 1.0 / scoreData.averageTime;
+	double k = 1000.0; // Adjust this scaling factor as necessary
 
-	return ((scoreData.correctAnswers / scoreData.totalAnswers) * CORRECT_ANSWER_WEIGHT * timeFunction) + (((double)scoreData.totalAnswers / scoreData.averageTime) * ANSWER_TIME_WEIGHT);
+	double score = (accuracy * 100) * (timeFactor * k);
+
+	return static_cast<int>(score);
 }
 
 int SqliteDatabase::getCountCallback(void* data, int argc, char** argv, char** azColName)
