ace5dc847656ebbc203f3075040e0664f9fd907f
Lior G <liorgleizer1@gmail.com>
2024-06-24 16:12:13 +0300

    #19 use CTOR

--- Trivia/RoomData.h
+++ Trivia/RoomData.h
@@ -11,6 +11,23 @@ struct RoomData {
 	unsigned int isActive;
 
 	std::time_t startTime;
+
+	//CTOR
+	RoomData(int id,
+		const std::string& name,
+		unsigned int maxPlayers,
+		unsigned int numOfQuestionsInGame,
+		unsigned int timerPerQuestion,
+		unsigned int isActive,
+		std::time_t startTime)
+		: id(id),
+		name(name),
+		maxPlayers(maxPlayers),
+		numOfQuestionsInGame(numOfQuestionsInGame),
+		timerPerQuestion(timerPerQuestion),
+		isActive(isActive),
+		startTime(startTime) {}
+
 	// https://json.nlohmann.me/api/macros/nlohmann_define_type_intrusive/
 	// in order for SerializeResponse to work
 	NLOHMANN_DEFINE_TYPE_INTRUSIVE_ONLY_SERIALIZE(RoomData, id, name, maxPlayers, numOfQuestionsInGame, timerPerQuestion, isActive);
