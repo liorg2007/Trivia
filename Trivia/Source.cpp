7c4949ed55a75bc770ca3554fb29a7255f980d4f
Itay Dali <itaydali8@gmail.com>
2024-07-06 03:04:54 +0300

    Removed tests from main

--- Trivia/Source.cpp
+++ Trivia/Source.cpp
@@ -1,31 +1,10 @@
 #include "Server.h"
 #include "WSAInitializer.h"
-#include "JsonRequestPacketDeserializer.h"
-#include "JsonResponsePacketSerializer.h"
-#include "QuestionsRetriever.h"
-
-#include "time.h"
-#include "AESCryptoAlgorithm.h"
-#include "aes.h"
 
 int main()
 {
-	Buffer key(CryptoPP::AES::DEFAULT_KEYLENGTH);
-	std::string message("testtesttesttesttesttesttesttesttest");
-	Buffer m(message.begin(), message.end());
-
-	AESCryptoAlgorithm algo;
-
-	auto a = algo.encrypt(m, key);
-	std::cout << (char*)a.data() << std::endl;
-	auto b = algo.decrypt(a, key);
-	
-	std::cout << (char*)b.data() << std::endl;
-	
-	/*
 	std::srand(std::time(nullptr)); // For randomly generating numbers using std::rand()
 	WSAInitializer wsaInit;
-	auto a = QuestionsRetriever::retrieveQuestions(5);
 	try
 	{
 		Server& server = Server::getInstance();
@@ -37,5 +16,5 @@ int main()
 	}
 
 	system("PAUSE");
-	return 0;*/
+	return 0;
 }
